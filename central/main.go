9571f5b7b6834146f4d9f3a6c0d36ba799890cf1
ItsArnavSh <its.arnav.sh@gmail.com>
2025-06-13 15:42:55 +0530

    Config

--- central/main.go
+++ central/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	setup "central/config"
+	"context"
+	"fmt"
+
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Print("Hello")
+	_ = context.Background()
+	logger, _ := zap.NewProduction()
+	defer logger.Sync()
+	setup.LoadConfig(logger)
+	fmt.Println(viper.GetString("docker.redis.port"))
 }
