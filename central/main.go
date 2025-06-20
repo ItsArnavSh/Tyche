518808ad341872f53c2a38b00b6af9b0ee207d81
its.arnav.sh <its.arnav.sh@gmail.com>
2025-06-20 10:41:25 +0530

    Changes

--- central/main.go
+++ central/main.go
@@ -4,6 +4,7 @@ import (
 	setup "central/config"
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
@@ -12,7 +13,17 @@ import (
 func main() {
 	_ = context.Background()
 	logger, _ := zap.NewProduction()
-	defer logger.Sync()
-	setup.LoadConfig(logger)
+	defer func() {
+		err := logger.Sync()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "logger sync error: %v\n", err)
+		}
+
+	}()
+	err := setup.LoadConfig(logger)
+	if err != nil {
+		logger.Error("Conifg Failed to Load", zap.Error(err))
+	}
+
 	fmt.Println(viper.GetString("docker.redis.port"))
 }
