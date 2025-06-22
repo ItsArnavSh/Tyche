d57b7cc34807783c729f19fe1981c78eeb85c256
its.arnav.sh <its.arnav.sh@gmail.com>
2025-06-22 11:54:01 +0530

    Rust GRPC Working

--- tech_signal/src/main.rs
+++ tech_signal/src/main.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Hello, world!");
+use tonic::{Request, Response, Status, transport::Server};
+
+mod proto {
+    tonic::include_proto!("stockrec");
+}
+
+use proto::rust_service_server::{RustService, RustServiceServer};
+use proto::{SendStockDataRequest, SendStockDataResponse};
+
+#[derive(Default)]
+pub struct RustyService;
+
+#[tonic::async_trait]
+impl RustService for RustyService {
+    async fn send_stock_data(
+        &self,
+        request: Request<SendStockDataRequest>,
+    ) -> Result<Response<SendStockDataResponse>, Status> {
+        let stocks = request.into_inner().stocks;
+
+        for stock in stocks {
+            println!(
+                "Got stock {}: open={}, close={}, high={}, low={}",
+                stock.name, stock.open, stock.close, stock.high, stock.low
+            );
+        }
+
+        let response = SendStockDataResponse { status: true };
+        Ok(Response::new(response))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50052".parse()?;
+    let service = RustyService::default();
+    println!("Starting server at 50052");
+    Server::builder()
+        .add_service(RustServiceServer::new(service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
 }
