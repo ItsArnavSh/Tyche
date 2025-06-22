41bd4c9dfd7217f2b00d00fa987b7e4a9f0159ee
its.arnav.sh <its.arnav.sh@gmail.com>
2025-06-22 20:40:57 +0530

    Changes

--- tech_signal/src/services/redis/redis.rs
+++ tech_signal/src/services/redis/redis.rs
@@ -1,8 +1,14 @@
 use redis::{Commands, Connection, RedisResult};
+
 pub struct RedisConn {
     pub conn: Connection,
 }
-
+pub struct Signal{
+   ticker:&str,
+   strategy:&str,
+   decision:&str
+    
+}
 impl RedisConn {
     pub fn new(url: &str) -> redis::RedisResult<Self> {
         let client = redis::Client::open(url).unwrap();
@@ -15,7 +21,10 @@ impl RedisConn {
     pub fn get_cache(&mut self, ticker: &str, strategy: &str) -> RedisResult<f32> {
         self.conn.get(format!("{ticker}~{strategy}"))
     }
-    pub fn push_signal(&mut self, data: &str) -> RedisResult<()> {
+    pub fn push_signal(&mut self, ticker: &str, strategy: &str, decision: &str) -> RedisResult<()> {
+        let signal = 
+        let data = serde_json::to_string(&signal).unwrap();
+
         self.conn.lpush("stocksignal", data)
     }
 }
