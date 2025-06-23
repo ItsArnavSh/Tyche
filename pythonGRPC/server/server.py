fc2afd0855ab2847a9fa0fed404ab6b421a469a9
its.arnav.sh <its.arnav.sh@gmail.com>
2025-06-24 01:26:06 +0530

    BackTesting GRPC Set UP

--- pythonGRPC/server/server.py
+++ pythonGRPC/server/server.py
@@ -1,6 +1,8 @@
+import time
 from grpc import aio
 import proto.pythonservice_pb2 as pb2
 import proto.pythonservice_pb2_grpc as pb2_grpc
+from services.backtest import GetDayDataStocks, get_days_between
 from services.newspaper import fetchArticle
 from sentence_transformers import SentenceTransformer
 from services.wordprocessor import generateKeywords
@@ -24,7 +26,14 @@ class PythonUtilServicer(pb2_grpc.PythonUtilServicer):
         keywords = generateKeywords(text)
         return pb2.GenerateKeywordsResponse(keywords=keywords)
 
-
+    async def StreamHistorical(self, request:pb2.StreamHistoricalRequest, context):
+        days:list[str] = get_days_between(request.startdate,request.enddate)
+        # The flow would be to get one days data, trim out the zeros and feed it to the stream, then repeat for all days
+        for day in days:
+            data = GetDayDataStocks(day,list(request.tickers))
+            for row in data:
+                yield row
+                time.sleep(0.5)
 async def serve():
     server = aio.server()
     pb2_grpc.add_PythonUtilServicer_to_server(PythonUtilServicer(), server)
