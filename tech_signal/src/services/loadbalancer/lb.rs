14a3ad565996755c85b66e46dfa9918e051da968
its.arnav.sh <its.arnav.sh@gmail.com>
2025-06-26 23:28:26 +0530

    Adding LoadBalancer

--- tech_signal/src/services/loadbalancer/lb.rs
+++ tech_signal/src/services/loadbalancer/lb.rs
@@ -1,8 +1,12 @@
 use std::{
     collections::{HashMap, VecDeque},
-    thread,
-    time::{self, SystemTime},
+    sync::Arc,
+    time::{Duration, SystemTime},
 };
+
+use tokio::{spawn, sync::Mutex, time::sleep};
+
+use crate::services::loadbalancer::strategies::strategy::Strategy;
 
 pub struct LoadBalancer {
     pub active_tasks: HashMap<String, Vec<u32>>,
@@ -10,51 +14,84 @@ pub struct LoadBalancer {
 }
 
 impl LoadBalancer {
-    pub fn new() -> Self {
-        //Trugger the queue_to_dict function
-
-        return LoadBalancer {
+    pub fn new() -> Arc<Mutex<Self>> {
+        Arc::new(Mutex::new(Self {
             active_tasks: HashMap::new(),
             time_queue: VecDeque::new(),
-        };
+        }))
     }
 
-    //Private Funcs
-    fn queue_to_dict(&mut self) {
+    pub fn start_process(&self, lb: Arc<Mutex<Self>>) {
+        let lb = lb.clone();
+        spawn(async move {
+            LoadBalancer::queue_to_dict(lb).await;
+        });
+    }
+    pub fn add_to_queue(&mut self, ticker: String, strat: Strategy) {
+        let time = {
+            if strat.alert {
+                strat.alert_candle_size
+            } else {
+                strat.candle_size
+            }
+        };
+        let new_time = add_time(time);
+        self.time_queue.push_back(TimedTask {
+            run_at: new_time,
+            ticker: ticker,
+            funcid: strat.functionid,
+        });
+    }
+    async fn queue_to_dict(lb: Arc<Mutex<Self>>) {
         loop {
-            let top = self.time_queue.pop_front();
-            match top {
-                Some(top) => self.add_to_dict(top.ticker, top.funcid),
-                None => thread::sleep(time::Duration::from_secs(1)),
+            let task_opt = {
+                let mut this = lb.lock().await;
+                this.time_queue.pop_front()
+            };
+
+            if let Some(top) = task_opt {
+                {
+                    let mut this = lb.lock().await;
+                    this.add_to_dict(top.ticker, top.funcid);
+                }
+            } else {
+                sleep(Duration::from_secs(1)).await; //Polling mode
+                continue;
             }
 
-            let top = self.time_queue.front();
-            match top {
-                Some(top) => sleep_until(top.run_at),
-                None => thread::sleep(time::Duration::from_secs(1)),
+            let maybe_next = {
+                let this = lb.lock().await;
+                this.time_queue.front().cloned()
+            };
+
+            match maybe_next {
+                Some(top) => sleep_until(top.run_at).await,
+                None => sleep(Duration::from_secs(1)).await,
             }
         }
     }
+
     fn add_to_dict(&mut self, ticker: String, func_id: u32) {
         self.active_tasks
             .entry(ticker)
             .or_insert(Vec::new())
-            .push(func_id)
+            .push(func_id);
     }
 }
 
+#[derive(Clone)]
 pub struct TimedTask {
     pub run_at: SystemTime,
     pub ticker: String,
     pub funcid: u32,
 }
-fn sleep_until(target: SystemTime) {
-    match target.duration_since(SystemTime::now()) {
-        Ok(duration) => {
-            thread::sleep(duration); // Sleep for remaining time
-        }
-        Err(_) => {
-            // Target time is already in the past; return immediately
-        }
+
+async fn sleep_until(target: SystemTime) {
+    if let Ok(duration) = target.duration_since(SystemTime::now()) {
+        sleep(duration).await;
     }
+}
+fn add_time(duration: u64) -> SystemTime {
+    let duration = Duration::from_millis(duration);
+    SystemTime::now() + duration
 }
--- tech_signal/src/services/loadbalancer/strategies/strategy.rs
+++ tech_signal/src/services/loadbalancer/strategies/strategy.rs
@@ -1,11 +1,6 @@
 pub struct Strategy {
     pub functionid: u32,
-    pub candle_size: u32, //In milliseconds
-    pub active_hours: Option<ActiveHours>,
+    pub candle_size: u64, //In milliseconds
     pub alert: bool,
-    pub alert_candle_size: u32, //0 if alert is set to false
-}
-pub struct ActiveHours {
-    pub start_hour: String,
-    pub end_hour: String,
+    pub alert_candle_size: u64, //0 if alert is set to false
 }
